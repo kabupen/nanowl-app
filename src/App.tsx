ddeda9bf47e688daa8d79313dd7c1158af0bfd06
Kosuke Takeda <kosuke050205@gmail.com>
2024-07-24 23:27:21 +0900

    update for material UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import HlsPlayer from "./components/HlsPlayer.jsx"
 // import './App.css'
 import axios from "axios";
+import { Container, TextField, Button, Typography, Box, Grid } from '@mui/material';
 
 function App() {
   const [url, setUrl] = useState("")
@@ -47,37 +48,51 @@ function App() {
 
 
   return (
-    <div>
-      <h1>NanoOwl Viwer</h1>
-      <div>
-        <p>1. VST の RTSP の URL を記入する</p>
-        <input
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder="Enter RTSP URL"
-        />
-        <button onClick={handleStart}>Start</button>
-      </div>
+    <Container>
+      <Typography variant="h4" gutterBottom>NanoOwl Viwer</Typography>
+      <Grid container spacing={2} alignItems="center" direction="row">
+        <Grid item>
+          <TextField
+            fullWidth
+            label="Enter RTSP URL"
+            variant="outlined"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter RTSP URL"
+          />
+        </Grid>
+        <Grid item>
+          <Button onClick={handleStart}>Start</Button>
+        </Grid>
+      </Grid>
       { streamUrl && (
         <div>
         <HlsPlayer url={streamUrl} />
-        <input 
-            type="text"
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="object name"
+            variant="outlined"
             value={objects}
             onChange={(e) => setObjects(e.target.value)}
             placeholder="ex. a man,a hat"
-        />
-        <input 
-          type="text"
-          value={thresholds}
-          onChange={(e) => setThresholds(e.target.value)}
-          placeholder="ex. 0.1, 0.2"
-        />
-        <button onClick={handleSubmit}>Submit</button>
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={thresholds}
+            onChange={(e) => setThresholds(e.target.value)}
+            placeholder="ex. 0.1, 0.2"
+          />
+        </Grid>
+        <Grid>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </Grid>
         </div>
       )}
-    </div>
+    </Container>
   )
 }
 
